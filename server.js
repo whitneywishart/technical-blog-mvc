c0a6fbc93a4a7e4ed48bea332ef065ac3441084f
whitneywishart <whitneywishart@gmail.com>
2023-04-10 17:40:01 -0700

    removing set port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
