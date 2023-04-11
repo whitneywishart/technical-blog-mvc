9b06dc5b5e1dc4617fe30d3507b413619db67a0a
whitneywishart <whitneywishart@gmail.com>
2023-04-10 18:24:43 -0700

    troubleshoot

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
