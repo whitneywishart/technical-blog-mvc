5ce73f8f5b84ed595c966e5e8daa2caef14af03d
whitneywishart <whitneywishart@gmail.com>
2023-04-11 14:53:39 -0700

    troubleshoot:

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -42,7 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  // app.listen(PORT, () => console.log('Now listening'));
-  app.listen(process.env.PORT || 3001);
-  console.log('Listening')
+  app.listen(PORT, () => console.log('Now listening'));
 });
